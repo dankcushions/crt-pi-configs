7a3cf05e09b60e1410b673dc8713e2ba2cb2d22b
cbransden <chris.bransden@objectway.com>
2017-02-14 16:46:34 +0000

    Added 1366x768 resolution

--- crt-pi-configs.py
+++ crt-pi-configs.py
@@ -5,10 +5,12 @@
 # * screen height (eg 1080)
 # example usage:
 # python crt-pi-configs.py mame2003 1920 1080
+# python crt-pi-configs.py mame2003 1366 768
 # python crt-pi-configs.py mame2003 1280 720
 # python crt-pi-configs.py mame2003 1280 1024
 # python crt-pi-configs.py mame2003 curvature
 # python crt-pi-configs.py fbalpha 1920 1080
+# python crt-pi-configs.py fbalpha 1366 768
 # python crt-pi-configs.py fbalpha 1280 720
 # python crt-pi-configs.py fbalpha 1280 1024
 # python crt-pi-configs.py fbalpha curvature
--- retropiebatch.bat
+++ retropiebatch.bat
@@ -1,9 +1,11 @@
 @echo off
 python crt-pi-configs.py mame2003 1920 1080
+python crt-pi-configs.py mame2003 1366 768
 python crt-pi-configs.py mame2003 1280 720
 python crt-pi-configs.py mame2003 1280 1024
 python crt-pi-configs.py mame2003 curvature
 python crt-pi-configs.py fbalpha 1920 1080
+python crt-pi-configs.py fbalpha 1366 768
 python crt-pi-configs.py fbalpha 1280 720
 python crt-pi-configs.py fbalpha 1280 1024
 python crt-pi-configs.py fbalpha curvature
